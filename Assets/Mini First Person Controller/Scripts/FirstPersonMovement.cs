105cd3fc45ed9b4ac3477771122f9d09d9216f2f
Jennyjaen <poohjenny710@kaist.ac.kr>
2024-08-12 16:11:14 +0900

    Move with arduino

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -5,6 +5,7 @@ public class FirstPersonMovement : MonoBehaviour
 {
     public float speed =10f;
     public float turnSpeed = 5f;
+    public SerialController serial;
 
     private bool leftKeydown = false;
     private bool rightKeydown = false;
@@ -19,7 +20,8 @@ public class FirstPersonMovement : MonoBehaviour
     private Quaternion rightRot;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
-
+    private float sum_x;
+    private float sum_y;
 
 
     void Awake()
@@ -37,12 +39,51 @@ public class FirstPersonMovement : MonoBehaviour
         leftRot = lPaddle.transform.localRotation;
         rightPos = rPaddle.transform.localPosition;
         rightRot = rPaddle.transform.localRotation;
+
+        serial = FindObjectOfType<SerialController>();
+        sum_x = 0;
+        sum_y = 0;
     }
 
 
 
     void Update()
     {
+
+        if(serial.x != 0 || serial.y != 0)
+        {
+
+            Debug.Log(sum_x + " " + sum_y);
+            if(serial.x < 0) { rigidbody.AddTorque(0, 10f * (serial.y/ 2200), 0);}
+            else { rigidbody.AddTorque(0, -10f * (serial.y / 2200), 0); }
+            if(serial.y > 0) {
+                rigidbody.AddForce(-1 * transform.right * 15f * (serial.y / 500)); 
+                if(serial.x >0) { rPaddle.transform.RotateAround(rPaddle.transform.GetChild(0).position, boat.transform.forward,  (serial.y/6 )); }
+                else { lPaddle.transform.RotateAround(lPaddle.transform.GetChild(0).position, boat.transform.forward, (serial.y/6)); }
+                if(sum_y < 0)
+                {
+                    sum_x = 0;
+                    sum_y = 0;
+                    rPaddle.transform.localPosition = rightPos;
+                    rPaddle.transform.localRotation = rightRot;
+                    lPaddle.transform.localPosition = leftPos;
+                    lPaddle.transform.localRotation = leftRot;
+                }
+            }
+            else
+            {
+                if (serial.x <0) { rPaddle.transform.RotateAround(rPaddle.transform.GetChild(0).position, boat.transform.forward, (-serial.y /6 )); }
+                else { lPaddle.transform.RotateAround(lPaddle.transform.GetChild(0).position, boat.transform.forward, (-serial.y /6)); }
+                if(sum_y > 0)
+                {
+                    sum_x = 0;
+                    sum_y = 0;
+                }
+            }
+            sum_x += serial.x;
+            sum_y += serial.y;
+
+        }
         // 키보드로 보트 조작 및 노 회전
         if (Input.GetKeyDown(KeyCode.LeftArrow)) { 
             leftKeydown = true;
@@ -77,9 +118,9 @@ public class FirstPersonMovement : MonoBehaviour
 
 
         //최고속도 조절
-        if(rigidbody.velocity.magnitude > 10.0f)
+        if(rigidbody.velocity.magnitude > 15.0f)
         {
-            rigidbody.velocity = rigidbody.velocity.normalized * 10.0f;
+            rigidbody.velocity = rigidbody.velocity.normalized * 15.0f;
         }
         if(rigidbody.angularVelocity.magnitude > 1.0f)
         {
